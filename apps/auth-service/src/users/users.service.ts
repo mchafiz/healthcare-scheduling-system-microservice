91a74c93e6585bdce5b0799a864d4450a0b95299
muhammad chaerul hafiz <mchaerulhafiz@gmail.com>
2026-06-05 10:20:27 +0700

    feat(auth): add PrismaService and UsersService

---
+++ apps/auth-service/src/users/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async create(email: string, hashedPassword: string) {
+    return this.prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+      },
+    });
+  }
+}
